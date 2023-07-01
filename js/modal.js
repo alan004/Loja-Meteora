069dde096c873c55129f738a81493fa722988022
alan04 <alandocarmoabilio@gmail.com>
2023-06-30 21:03:32 -0300

    feat: (newsletter) adição da modal

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector(".modal");
+const openModalBtn = document.querySelector(".open");
+const closeModalBtn = document.querySelector(".close");
+
+openModalBtn.addEventListener("click", () => modal.showModal());
+closeModalBtn.addEventListener("click", () => modal.close());
