a3ce59368216cc8645bc596e36ccc63615c3f965
alan04 <alandocarmoabilio@gmail.com>
2023-07-03 20:29:48 -0300

    feat: (produtos) modelo da modal

--- js/modal.js
+++ js/modal.js
@@ -2,5 +2,12 @@ const modal = document.querySelector(".modal");
 const openModalBtn = document.querySelector(".open");
 const closeModalBtn = document.querySelector(".close");
 
+const modalProduto = document.querySelector(".modal--produto");
+const openModalBtnProduto = document.querySelector(".open--produto");
+const closeModalBtnProduto = document.querySelector(".close--produto");
+
+
 openModalBtn.addEventListener("click", () => modal.showModal());
 closeModalBtn.addEventListener("click", () => modal.close());
+openModalBtnProduto.addEventListener("click", () => modalProduto.showModal());
+closeModalBtnProduto.addEventListener("click", () => modalProduto.close());
