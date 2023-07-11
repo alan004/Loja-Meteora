dea1dac56c1dd3e4e0a060dd5da7ed5e2f10a8e3
alan04 <alandocarmoabilio@gmail.com>
2023-07-11 20:06:03 -0300

    feat: (slider) Autoplay

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,10 @@
 export const swiper = new Swiper('.swiper', {
     // Optional parameters
      loop: true,
-  
+     autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
